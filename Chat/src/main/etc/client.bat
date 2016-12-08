f29a97d52fbd3c09e948145f01d97ea659aa86a4
Dmitry Deshevoy <mityada@gmail.com>
2016-12-09 00:57:38 +0300

    Update example launch scripts

--- Chat/src/main/etc/client.bat
+++ Chat/src/main/etc/client.bat
@@ -5,8 +5,10 @@ set hostname=10.0.0.1
 
 rem internal name of the server - same as Openfire setting
 set serverName=10.0.0.1
+rem room to connect to
+set room=neerc
 set username=admin
 set password=admin
 
 
-@start java -Dserver.host=%hostname%   -Dserver.hostname=%serverName%  -Dusername=%username% -Dpassword=%password% -Dconsole.encoding=CP866 -jar chat-client.jar
+@start java -Dserver.host=%hostname% -Dserver.hostname=%serverName% -Droom=%room% -Dusername=%username% -Dpassword=%password% -Dconsole.encoding=CP866 -jar chat-client.jar
--- Chat/src/main/etc/client.sh
+++ Chat/src/main/etc/client.sh
@@ -4,8 +4,10 @@
 hostname=10.0.0.1
 # internal name of the server - same as Openfire setting
 serverName=10.0.0.1
+# room to connect to
+room=neerc
 username=admin
 password=admin
 
 
-java -Dserver.host=$hostname  -Dserver.hostname=$serverName -Dusername=$username -Dpassword=$password -Dconsole.encoding=UTF-8 -jar chat-client.jar
+java -Dserver.host=$hostname  -Dserver.hostname=$serverName -Droom=$room -Dusername=$username -Dpassword=$password -Dconsole.encoding=UTF-8 -jar chat-client.jar
