0036c50c2d42ea8af03f7b2dae080f2ddf934dc4
Dmitry Deshevoy <mityada@gmail.com>
2017-12-07 23:38:02 +0300

    Add URL to ChatMessage

--- src/main/java/ru/ifmo/neerc/chat/ChatMessage.java
+++ src/main/java/ru/ifmo/neerc/chat/ChatMessage.java
@@ -12,6 +12,7 @@ public class ChatMessage implements Comparable<ChatMessage>, Serializable {
         Pattern.compile("^(?:([a-zA-Z0-9%]+)>)? *(#+|\\?+|!+)?(.*)$", Pattern.DOTALL);
 
     private String text;
+    private String url;
     private UserEntry user;
     private String to;
     private Date date;
@@ -27,6 +28,11 @@ public class ChatMessage implements Comparable<ChatMessage>, Serializable {
     }
 
     public ChatMessage(String text, UserEntry user, String to, Date date) {
+        this(text, null, user, to, date);
+    }
+
+    public ChatMessage(String text, String url, UserEntry user, String to, Date date) {
+        this.url = url;
         this.user = user;
         this.to = to;
         this.date = date;
@@ -47,6 +53,10 @@ public class ChatMessage implements Comparable<ChatMessage>, Serializable {
 
     public String getText() {
         return text;
+    }
+
+    public String getUrl() {
+        return url;
     }
 
     public void setType(Type type) {
